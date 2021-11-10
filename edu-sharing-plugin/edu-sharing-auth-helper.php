7d6fbeecfa5864a8b552983a5bf04a46451a4042
Torsten Simon <simon@edu-sharing.net>
2021-11-10 17:35:49 +0100

    rename

--- edu-sharing-plugin/edu-sharing-auth-helper.php
+++ edu-sharing-plugin/edu-sharing-auth-helper.php
@@ -7,13 +7,13 @@ class AppAuthException extends Exception {
         parent::__construct($this->getExplanation($message));
     }
     private function getExplanation($message) {
-        $SOAP_ERRORS = [
+        $KNOWN_ERRORS = [
             "the timestamp sent by your client was too old. Please check the clocks of both servers or increase the value 'message_offset_ms'/'message_send_offset_ms' in the app properties file"
             => ["MESSAGE SEND TIMESTAMP TO OLD", "MESSAGE SEND TIMESTAMP newer than MESSAGE ARRIVED TIMESTAMP"],
             "The ip your client is using for request is not known by the repository. Please add the ip into your 'host_aliases' app properties file"
             => ["INVALID_HOST"]
         ];
-        foreach($SOAP_ERRORS as $desc => $keys) {
+        foreach($KNOWN_ERRORS as $desc => $keys) {
             foreach($keys as $k) {
                 if(strpos($message, $k) !== FALSE)
                 return $desc . '(' . $message . ')';
